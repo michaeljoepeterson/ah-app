90224b91bd3ed17f31469775928e3477afc8ef80
michael peterson <michaeljoepeterson@gmail.com>
2021-08-25 18:15:19 -0600

    initial model for custom field value hooked up to custom field

--- src/app/modules/custom-forms/models/custom-field-value.ts
+++ src/app/modules/custom-forms/models/custom-field-value.ts
@@ -1,14 +1,45 @@
 import { BaseModel } from "../../../models/baseModel";
 import { CustomField } from "./custom-field";
 import { fieldTypes } from "../constants";
+import { User } from "../../../models/users/user";
+import { CustomForm } from "./custom-form";
+import { PatientFile } from "../../client-dash/models/patient-file";
+
+export interface IValue{
+    stringValue:string;
+    numberValue:number;
+    arrayValue:any[];
+    objectValue:any;
+    dateValue:Date;
+}
 
 /**
  * model the custom field value
  */
 export class CustomFieldValue extends BaseModel{
-    value:any = null;
+    value:IValue = {
+        stringValue:null,
+        numberValue:null,
+        arrayValue:[],
+        objectValue:null,
+        dateValue:null,
+    };
     customFieldId:string = null;
     customField:CustomField = null;
+    name:string = null;
+    id:string = null;
+    owner:User = null;
+    createdAt:Date = null;
+    ancestorSections:string[] = [];
+    parentSection:string = null;
+    parentForm:CustomForm = null;
+    sortOrder:number = 0;
+    fieldType:string = null;
+    fieldOptions:any[] = null;
+    min:string = null;
+    max:string = null;
+    parentField:CustomField = null;
+    parentFile:PatientFile = null;
 
     constructor(data?:any){
         super();
@@ -20,7 +51,7 @@ export class CustomFieldValue extends BaseModel{
     initFieldValue(data:any){
         this.init(data);
         if(data.value && this.customField.fieldType === fieldTypes.date){
-            this.value = new Date(data.value);
+            this.value.dateValue = new Date(data.value);
         }
         //init empty field checkbox vals
         if(this.customField?.fieldOptions?.length > 0 && this.customField?.fieldType === fieldTypes.checkbox && !this.value){
@@ -29,11 +60,11 @@ export class CustomFieldValue extends BaseModel{
     }
     
     initCheckboxValues(){
-        this.value = [];
-        this.customField.fieldOptions.forEach(option => this.value.push(false));
+        this.value.arrayValue = [];
+        this.customField.fieldOptions.forEach(option => this.value.arrayValue.push(false));
     }
 
     initDate(value?:any){
-        this.value = !value ? new Date() : new Date(value);
+        this.value.dateValue = !value ? new Date() : new Date(value);
     }
 }
