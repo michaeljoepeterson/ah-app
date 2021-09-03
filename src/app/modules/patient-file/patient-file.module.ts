ec29616e1b363006438e4b9b225867593f7ffc7d
michael peterson <michaeljoepeterson@gmail.com>
2021-09-03 10:00:49 -0600

    checkout

--- src/app/modules/patient-file/patient-file.module.ts
+++ src/app/modules/patient-file/patient-file.module.ts
@@ -14,6 +14,7 @@ import { PatientCustomValueComponent } from './components/patient-custom-value/p
 import { ImageSlideshowComponent } from './components/details/image-slideshow/image-slideshow.component';
 import { MatIconModule } from '@angular/material/icon';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FileDownloadsComponent } from './components/file-downloads/file-downloads.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     PatientCustomFormComponent,
     PatientCustomSectionComponent,
     PatientCustomValueComponent,
-    ImageSlideshowComponent
+    ImageSlideshowComponent,
+    FileDownloadsComponent
   ],
   imports: [
     CommonModule,
